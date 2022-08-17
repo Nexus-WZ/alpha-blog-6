604b55601e201cbbc4fdeb0bd0456148813134ab
Aslan Simaiev <aslan.simaiev@terminus.com>
2022-08-17 22:12:09 +0300

    Added users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
 
-  def new
-  @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  
   def edit
     @user = User.find(params[:id])
   end
